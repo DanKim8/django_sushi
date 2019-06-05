d6e1b7e51c3403cbb000860e4fea58bb7ee2519c
root <root@DESKTOP-TRUFV8T.localdomain>
2019-06-05 02:52:22 -0400

    Updated Views

--- apps/views.py
+++ apps/views.py
@@ -8,7 +8,25 @@ from .models import Appetizer, Sushi, Sashimi, A_La_Carte, Rolls, Vegetarian_Rol
 def index(request):
     return render(request, "main/index.html")
 
-#def menu(request):
-    
+def findDistinctVals(table, column):
+    return table.objects.values_list(column, flat = true).distinct()
 
+#List the things on the menu
+def getEntities(Entity, EntityName):
+
+
+
+
+
+#def menu(request):
+    context = {
+	    "Appetizer": getEntities(Appetizer, "Appetizer"),
+		"Sushi": getEntities(Sushi, "Sushi"),
+		"Sashimi": getEntities(Sashimi, "Sashimi"),
+		"A_La_Carte": getEntities(A_La_Carte, "A_La_Carte"),
+		"Rolls": getEntities(Rolls, "Rolls"),
+		"Vegetarian_Rolls": getEntities(Vegetarian_Rolls, "Vegetarian_Rolls"),
+		"Special_Rolls": getEntities(Special_Rolls, "Special_Rolls"),
+		"WB_Special_Rolls": getEntities(WB_Special_Rolls, "WB_Special_Rolls"),
+	}
 #def home(request):   
